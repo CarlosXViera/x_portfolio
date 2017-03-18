b17f602c2dc840b7378d386367b6654d7e165418
Carlos Viera <CarlosXViera@gmail.com>
2017-03-17 20:29:45 -0700

    orientation allows movement relative to orientation of mobile

--- src/components/Glasses.jsx
+++ src/components/Glasses.jsx
@@ -40,18 +40,19 @@ export default class Glasses extends React.Component {
 	}
 
 	componentWillReceiveProps(nextProps) {
-		this.renderGlasses(nextProps.orientation)
+		this.renderGlasses(nextProps.orientation);
 	}
 
 	renderGlasses(orientation) {
 		this.state.g.html(this.el.bottom1);
-		let xCoord, yCoord;
-		if(orientation === 'landscape'){
+		let xCoord,
+			yCoord;
+		if (orientation === 'landscape') {
 			this.state.g.attr('transform', 'translate(420, 300)')
 			xCoord = 420;
 			yCoord = 300;
 			this.float(this.state.g, xCoord, yCoord, false, this.showUpDown);
-		}else {
+		} else {
 			this.state.g.attr('transform', 'translate(130, 420)')
 			xCoord = 130;
 			yCoord = 420;
@@ -73,7 +74,8 @@ export default class Glasses extends React.Component {
 		}
 	}
 
-	sprite(e) {
+	portraitGyro(e) {
+
 		if (e.gamma < 20 && e.gamma > -20) {
 			this.state.g.html(this.el.front);
 		}
@@ -113,6 +115,50 @@ export default class Glasses extends React.Component {
 		if (e.beta > 110) {
 			this.state.g.html(this.el.bottom3)
 		}
+	}
+
+	landscapeGyro(e) {
+		// d3.selectAll('text').remove();
+		// let a = d3.select('svg').append('text').text(e.gamma).attr('font-size', 30).attr('x', 0).attr('y', 40);
+
+		if (e.beta > -12 && e.beta < -9) {
+			this.state.g.html(this.el.right3);
+		} else if (e.beta > -8 && e.beta < -5) {
+			this.state.g.html(this.el.right2);
+		} else if (e.beta > -4 && e.beta < -3.5) {
+			this.state.g.html(this.el.right1);
+		} else if (e.beta > -3 && e.beta < 3) {
+			this.state.g.html(this.el.front);
+		} else if (e.beta > 4 && e.beta < 7) {
+			this.state.g.html(this.el.left1)
+		} else if (e.beta > 8 && e.beta < 11) {
+			this.state.g.html(this.el.left2)
+		} else if (e.beta > 12 && e.beta < 14) {
+			this.state.g.html(this.el.left3)
+		} else if (e.gamma > -55 && e.gamma < -46) {
+			this.state.g.html(this.el.top1);
+		} else if (e.gamma > -45 && e.gamma < -36) {
+			this.state.g.html(this.el.top2);
+		} else if (e.gamma > -35 && e.gamma < -26) {
+			this.state.g.html(this.el.top3);
+		} else if (e.gamma < 90 && e.gamma > 80) {
+			this.state.g.html(this.el.bottom1);
+		} else if (e.gamma < 79 && e.gamma > 69) {
+			this.state.g.html(this.el.bottom2);
+		} else if (e.gamma < 68 && e.gamma > 58) {
+			this.state.g.html(this.el.bottom3);
+		}
+
+	}
+
+	sprite(e) {
+		if(this.props.orientation === 'landscape'){
+			this.landscapeGyro(e);
+		}else {
+			this.portraitGyro(e);
+		}
+
+
 	}
 
 	render() {
