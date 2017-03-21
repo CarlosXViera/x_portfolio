5843a02bc9170667b2f018f6e6d80e9b8d004f82
Carlos Viera <CarlosXViera@gmail.com>
2017-03-20 19:49:08 -0700

    added tabs - resizing issue

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,41 +1,43 @@
 import React, {PropTypes} from 'react'
-import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Link, Switch, NavLink} from 'react-router-dom'
 import Works from 'Works'
 import Contact from 'Contact'
 import About from 'About'
 
 export default class Nav extends React.Component {
 
-    constructor(props) {
-        super(props)
+	constructor(props) {
+		super(props)
 
-        this.state = {};
-    }
+		this.state = {};
+	}
 
-    componentWillMount() {}
+	componentWillMount() {}
 
-    render() {
+	render() {
 
-        return (
-            <Router>
-                <div>
-                    <div className="Main">
-                        <Route path='/contacts' component={Contact}/>
-                        <Route path='/works' component={Works}/>
-                        <Route exact path='/' component={About}/>
-                    </div>
-                    <div className="tab contacts">
-                        <Link to="/contacts" className="tabText">Contact</Link>
-                    </div>
-                    <div className="tab works">
-                        <Link to="/works" className="tabText">Works</Link>
-                    </div>
-                    <div className="tab about">
-                        <Link to="/about" className="tabText">About</Link>
-                    </div>
+		return (
+			<Router>
+				<div className='Main'>
+					<div className="content">
+						<Route path='/contacts' component={Contact}/>
+						<Route path='/works' component={Works}/>
+						<Route exact path='/' component={About}/>
+					</div>
+					<NavLink to="/contacts" className="tabText">
+						<div className="tab contacts">Contact</div>
+					</NavLink>
 
-                </div>
-            </Router>
-        )
-    }
+					<NavLink to="/works" className="tabText">
+						<div className="tab works">Works</div>
+					</NavLink>
+
+					<NavLink to="/about" className="tabText">
+						<div className="tab about">About</div>
+					</NavLink>
+
+				</div>
+			</Router>
+		)
+	}
 }
