7f09f6f8f7c3ce2621d8fea650a238a4ce3e5702
Carlos <cx@viera.io>
2017-06-15 15:10:46 -0400

    finally got hexagon to work. needs refactor

--- src/components/Hexagons.jsx
+++ src/components/Hexagons.jsx
@@ -30,7 +30,6 @@ export default class Hexagons extends React.Component {
 	}
 
 	componentWillMount() {
-		console.log('mounting');
 	}
 
 	shouldComponentUpdate() {
@@ -59,7 +58,7 @@ export default class Hexagons extends React.Component {
 				let calculatedHSpacing = (hSize + hSpacing) * hHexagons,
 					calculatedVSpacing = (hSize + vSpacing) * vHexagons;
 
-				translate = vHexagons % 2 ? `${calculatedHSpacing}, ${calculatedVSpacing}` :
+				translate = vHexagons % 2 === 0 ? `${calculatedHSpacing}, ${calculatedVSpacing}` :
 					`${calculatedHSpacing + offset}, ${calculatedVSpacing}`;
 
 				translatedHexagon = selection.append('g')
@@ -70,7 +69,6 @@ export default class Hexagons extends React.Component {
 
 				translatedHexagon.select('polygon')
 				.attr('class','hexagon').on('click', function(d){
-					console.log(this.parentNode)
 					select(this).attr('stroke', 'yellow');
 				});
 
@@ -102,23 +100,29 @@ export default class Hexagons extends React.Component {
 				}
 			}
 
+			let x = start.x,
+				y = start.y,
+				fn = (direction === 'right') ? addRight : addLeft;
+				arr[start.y][start.x].attr('stroke', 'yellow');
+				let coll = {right: start}
 
-			let x = start.x;
-			let y = start.y;
-			let fn = (direction === 'right') ? addRight : addLeft;
-
-			for(let j = 0; j < times; j++){
+			for(let j = 1; j < times; j++){
 				let r = fn([y,x]);
 				y = r.y;
 				x = r.x;
-
 				arr[y][x].attr('stroke', 'yellow');
-				console.log(y,x);
+				if(j === times - 1) coll['left'] = {y, x};
 			}
+
+			return coll;
 		}
 
 		function isEven(n){
 			return (n % 2 === 0);
+		}
+
+		function adjust(obj, direction, adj){
+			return (direction === 'r') ? {x: obj.x + adj, y: obj.y} : {x: obj.x - adj, y: obj.y}
 		}
 
 		/* multiply these amounts by the amount of layers. */
@@ -128,58 +132,90 @@ export default class Hexagons extends React.Component {
 		let isTopDone = false;
 		let isBtmDone = false;
 		let hexArr = this.hexagonArray;
-		let rings = 1;
- 		let nextLayer;
+		let rings = 30;
 		let coords = {
-			y: Math.round(hexArr.length / 2),
-			x: Math.round(hexArr[0].length / 2)
+			y: Math.floor(hexArr.length / 2),
+			x: Math.floor(hexArr[0].length / 2)
 		}
-		// even add to x odd do nothing;
-		//let topLayer = (isEven(coords.y)) ?
+		let origin = coords;
+		let leftSide = [];
+		let rightSide = [];
 
-		hexArr[coords.y][coords.x].attr('stroke', 'black');
 
-		let isRowEven = !isEven(coords.y);
+		hexArr[coords.y][coords.x].attr('stroke', 'red');
 
 		for(let i = 0; i < rings; i++){
-			let topLayer = null;
-			let level = i + 1;
+			let topLayer = {},
+				level = i + 1;
+
+			if(isEven(origin.y)){
+				topLayer['y'] = origin.y - 1;
+				topLayer['x'] = origin.x + 1;
+			} else {
+				topLayer['y'] = origin.y - 1;
+				topLayer['x'] = origin.x;
+			}
+
+			let bottomLayer = {
+				...topLayer
+			}
+			bottomLayer.y += (layers - 1);
+
 			for(let j = 0; j < layers; j++){
 				if(isTopDone === false){
-					/* if it's odd add to the right. if it's even add 1 to x, then add to the right */
-						console.log(isEven(i), i)
-						topLayer = (isEven(i)) ? {y:coords.y - level, x:coords.x + level} : {y:coords.y - level, x:coords.x + level};
-						nextLayer = {
-							y: topLayer.y + 1,
-							x: topLayer.x
-						};
-						chooseHex(topLayer, 'left', topBtmAmount, hexArr);
 						isTopDone = true;
-				} else if(isBtmDone === false){
+						if(topLayer.y < 0) continue;
+						let sides = chooseHex(topLayer, 'left', topBtmAmount, hexArr);
 					/* if it's odd add to the right. if it's even add 1 to x, then add to the right */
-					let bottomLayer = {y: topLayer.y + (layers - 1), x: topLayer.x}
-					chooseHex(bottomLayer, 'left', topBtmAmount, hexArr);
+						leftSide.push(sides.left);
+						rightSide.push(sides.right);
+				} else if(isBtmDone === false){
 					isBtmDone = true;
+					/* if it's odd add to the right. if it's even add 1 to x, then add to the right */
+					if(bottomLayer.y > hexArr.length -1) continue;
+					let sides = chooseHex(bottomLayer, 'left', topBtmAmount, hexArr);
+					leftSide.push(sides.left);
+					rightSide.push(sides.right);
 				} else if(isTopDone && isBtmDone){
+					if(level === 1){
+						let adjustedleft = adjust(origin, 'l', 1);
+						let adjustedright = adjust(origin, 'r', 1);
+						let left = chooseHex(adjustedleft, 'right', 1, hexArr).right;
+						let right = chooseHex(adjustedright, 'right', 1, hexArr).right;
+						rightSide.push(right);
+						leftSide.push(left);
+						break;
+					}
+					let rmr = rightSide.splice(rightSide.length - 2, 2);
+					let rml = leftSide.splice(leftSide.length - 2, 2);
+					rightSide = rightSide.map((obj) => {
+						let adjusted = adjust(obj, 'r', 1);
+						chooseHex(adjusted, 'right', 1, hexArr)
+						return adjusted;
+					});
+
+					leftSide = leftSide.map((obj) => {
+						let adjusted = adjust(obj, 'l', 1);
+						chooseHex(adjusted, 'left', 1, hexArr)
+						return adjusted;
+					});
+
+					rightSide = rightSide.concat(rmr);
+					leftSide = leftSide.concat(rml);
+					break;
+
 					/* both top and bottom are done. */
 					/* check if y-coordinate is odd. */
-					let startPoint = nextLayer;
-					startPoint.x--
-					chooseHex(startPoint, 'left', 1, hexArr);
-					chooseHex(startPoint, 'right', 1, hexArr);
-					startPoint.y++;
-					nextLayer = startPoint;
-
 				}
+
 			}
 			/* change values here */
-			layers += 2;
-			topBtmAmount++;
+			origin = topLayer;
 			isTopDone = false;
 			isBtmDone = false;
-
+			topBtmAmount++;
+			layers += 2;
 		}
-
 	}
 
 	renderHexagons(props){
@@ -189,8 +225,7 @@ export default class Hexagons extends React.Component {
 }
 
 componentDidMount(){
-	console.log('did Mount');
-	console.log(document.getElementById('1630'))
+
 }
 
 	render() {
